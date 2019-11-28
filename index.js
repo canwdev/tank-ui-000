026cd1a6d9011fec14915abcd4b42c822766f3f2
canwdev <c2097@live.com>
2019-11-28 11:52:52 +0800

    fix

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import HelloWorld from './src/components/HelloWorld'
 import Switcher from './src/components/Switcher'
 
 // 存储组件列表
-const components = [HelloWorld, Switch]
+const components = [HelloWorld, Switcher]
 
 // 定义 install 方法，接收 Vue 作为参数。如果使用 use 注册插件，则所有的组件都将被注册
 const install = function (Vue) {
